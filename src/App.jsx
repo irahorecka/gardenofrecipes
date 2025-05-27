b4934a515db3177676b69e29c7a3b43b3f35b996
Ira Horecka <ira89@icloud.com>
2025-05-26 17:29:09 -0700

    rm Directory.jsx - have directory / index on the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-// src/App.jsx
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
-import Directory from "./pages/Directory.jsx";
 import Home from "./pages/Home.jsx";
 import Recipe from "./pages/Recipe.jsx";
 
@@ -10,7 +8,6 @@ export default function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<Directory />} />
         <Route path="/recipes/:slug" element={<Recipe />} />
       </Routes>
     </Layout>
